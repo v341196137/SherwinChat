27f2977f564f66b4a4ff39c0b308cc3e823bdcdf
Sherwin Chiu <sherwin.chiu89@gmail.com>
2021-03-07 12:41:38 -0500

    Implementing Express.js

--- server.js
+++ server.js
@@ -2,12 +2,28 @@
  * @fileoverview Server for SherwinChat
  */
 
-var http = require("http");
-var dt = require("./datetime.js");
+const http = require("http");
+const express = require("express");
+const dt = require("./datetime.js");
 
-http.createServer(function (req, res){
+const app = express();
+const port = 5000;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+"/index.html");
+    res.send("SherwinChat access time: " + dt.myDateTime());
+}).listen(port);
+console.log("Node server started on port "+port);
+
+/*
+server = http.createServer(function (req, res){
     res.writeHead(200, {"Content-Type": "text/plain"}); 
     let url = req.url;
     res.write("SherwinChat access time: " + dt.myDateTime());
     res.end();
-}).listen(8080);
+});
+
+server.listen(port, function(){
+    console.log('Node server created on port '+port);
+})
+*/
