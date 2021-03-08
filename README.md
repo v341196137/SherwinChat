03bd063302043171863267547fe928cdad81e30b
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-03-07 19:05:24 -0500

    added socketio but... mime types are being bad >:(

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SherwinChat
 
 A chat application made to honor Sherwin Chiu, god of capacitors!!!
+
+## Dependancies
+
+* express
+* socket.io
--- server.js
+++ server.js
@@ -2,28 +2,42 @@
  * @fileoverview Server for SherwinChat
  */
 
-const http = require("http");
 const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const dt = require("./datetime.js");
 
-const app = express();
 const port = 5000;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname+"/index.html");
-    res.send("SherwinChat access time: " + dt.myDateTime());
-}).listen(port);
+    // res.send("SherwinChat access time: " + dt.myDateTime());
+}).listen(port, () =>{
+    //listening
+});
 console.log("Node server started on port "+port);
 
-/*
-server = http.createServer(function (req, res){
-    res.writeHead(200, {"Content-Type": "text/plain"}); 
-    let url = req.url;
-    res.write("SherwinChat access time: " + dt.myDateTime());
-    res.end();
+io.on('connection', (socket) => {
+    console.log("user connected at " + socket);
+    socket.on('disconnect', () =>{
+        console.log("user disconnected");
+    })
 });
 
-server.listen(port, function(){
-    console.log('Node server created on port '+port);
-})
-*/
+// http.listen(port, () => {
+//     console.log("listening");
+// });
+
+
+// server = http.createServer(function (req, res){
+//     res.writeHead(200, {"Content-Type": "text/plain"}); 
+//     let url = req.url;
+//     // res.write("SherwinChat access time: " + dt.myDateTime());
+//     res.end();
+// });
+
+// http.listen(port, function(){
+//     console.log('Node server created on port ' + port);
+// });
+
