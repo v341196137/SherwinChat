05e112ba74d2431ec404cef6ba688d32d917dc7c
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-03-10 20:55:40 -0500

    RGB CYCLIC!!!! :DDDD

--- js/index.js
+++ js/index.js
@@ -9,9 +9,17 @@ var form = document.getElementById('form');
 var input = document.getElementById('message-box');
 var messages = document.getElementById("messages");
 
+let red = 255;
+let green = 0;
+let blue = 0;
+let dr = 0;
+let dg = 1;
+let db = 0;
+
 function start(){
     socket.emit("hello", "yes");
     console.log(socket);
+    updateColours();
 }
 function emitter(res, msg){
     socket.emit(res, msg);
@@ -41,3 +49,46 @@ form.addEventListener('submit', function(e) {
     }
 });
 
+function updateColours() {
+    let root = document.documentElement;
+    red += dr;
+    green += dg;
+    blue += db;
+    if(red >= 255 && green <= 0){
+      dr = 0;
+      dg = 1;
+      db = 0;
+    }else if(red <= 0 && green >= 255 && blue <= 0){
+      dr = 0;
+      dg = 0;
+      db = 1;
+    }else if(red <= 0 && green <= 0 && blue >= 255){
+      dr = 1;
+      dg = 0;
+      db = 0;
+    }else if(red >= 255 && green >= 255){
+      dr = -1;
+      dg = 0;
+      db = 0;
+    }else if(blue >= 255 && green >= 255){
+      dr = 0;
+      dg = -1;
+      db = 0;
+    }else if(red >= 255 && blue >= 255){
+      dr = 0;
+      dg = 0;
+      db = -1;
+    }else if(red >= 255 && blue <= 0){
+      dr = 0;
+      dg = 1;
+      db = 0;
+    }
+    root.style.setProperty('--red', red);
+    root.style.setProperty('--green', green);
+    root.style.setProperty('--blue', blue);
+    root.style.setProperty('--invRed', 255 - red);
+    root.style.setProperty('--invGreen', 255 - green);
+    root.style.setProperty('--invBlue', 255 - blue);
+    setTimeout(updateColours, 50);
+  }
+  updateColours();
